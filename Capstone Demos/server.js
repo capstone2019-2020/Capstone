584b21668a8c27f86231131da2b0b6fb30ad8093
Patricia <patsmrkt@gmail.com>
2019-08-10 21:26:24 -0400

    Committing graph UI demo - cytoscape

---
+++ Capstone Demos/server.js
@@ -0,0 +1,19 @@
+const express 	= require('express');
+const morgan		= require('morgan');
+const bodyParser	= require('body-parser');	// pull info from HTML POST (express4)
+const methodOverride = require('method-override');
+
+const app 		= express();
+app.use(express.static('.'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+
+app.get('/cytoscape', function(req, res) {
+  res.sendfile('cytoscape-demo/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+app.listen(8080);
+console.log("App listening on port 8080");
