018ddc9eea780dad969da2c6c5fd45a2df4ccee2
Patricia <patsmrkt@gmail.com>
2019-09-11 10:53:27 -0400

    Committing getUserInput() function

---
+++ src/m1.js
@@ -0,0 +1,58 @@
+/**
+ * Import the required libraries
+ */
+const algebra = require('algebra.js');
+const readline = require('readline');
+
+/**
+ * Retrieve user inputs:
+ * - n - # of equations
+ * - List of equations
+ * - Start node
+ * - End node
+ * Note: Assumes that the user will enter valid values
+ */
+function getUserInput() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  let n = 0;
+  let equations = [];
+  let startNode = '';
+  let endNode = '';
+
+  // Get the user inputs
+  rl.question('Please type in the number of equations: ', (ans) => {
+    n = parseInt(ans);
+    rl.on('line', (input) => {
+      equations.push(algebra.parse(input));
+
+      if (equations.length === n) {
+        rl.question('Please type in start node: ', (ans) => {
+          startNode = ans;
+          rl.question('Please type in end node: ', (end) => {
+            endNode = end;
+            rl.close();
+          });
+        });
+      }
+    });
+  });
+
+  /*
+   * For testing purposes, print out the variable values
+   * Note: Once all of the tasks are completed, will have to add the data processing and function calls here
+   */
+  rl.on('close', () => {
+    console.log('------------------------------------');
+    console.log(`RETRIEVED USER INPUTS: `);
+    console.log(`n: ${n} `);
+    console.log(`equations: `);
+    equations.forEach((eq) => console.log(eq.toString()));
+    console.log(`start node: ${startNode}`);
+    console.log(`end node: ${endNode}`);
+    console.log('------------------------------------');
+  });
+}
