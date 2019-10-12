e847f7bfc6725bb876e13e32f45a42c9aa653428
Jimmy Li <dfjimmy.li@gmail.com>
2019-10-11 22:19:59 -0400

    UI working - skeleton

--- src/index.js
+++ src/index.js
@@ -2,6 +2,52 @@ const inputForm = document.getElementById("input-form");
 const equations = document.getElementById("equations");
 inputForm.onsubmit = generate;
 
+
+function validate(schema) {
+  if (!schema.equations.length || !schema.start_node || !schema.end_node) {
+    alert('please fill in all fields!');
+    return;
+  }
+
+  let exprs, lhs, rhs;
+  let validStart = false, validEnd = false;
+  let valid = true;
+  schema.equations.forEach((eqn, i) => {
+    if (!eqn.match(/^[a-zA-Z0-9_\/*\-+\s=]*$/)) {
+      valid = false;
+      alert(`equation # ${i} contains unaccepted characters`);
+    }
+    if (!eqn.includes('=')) {
+      valid = false;
+      alert(`equation # ${i} must contain =`);
+    }
+    exprs = eqn.split('=');
+    if (exprs.length > 2) {
+      valid = false;
+      alert(`equation # ${i} contains more than 1 equals`);
+    }
+    ([lhs, rhs] = exprs);
+    if (lhs.trim().split(' ').length !== 1) {
+      valid = false;
+      alert(`equation # ${i} contains more than 1 term on LHS`);
+    }
+
+    if (eqn.includes(schema.start_node)) {
+      validStart = true;
+    }
+    if (eqn.includes(schema.end_node)) {
+      validEnd = true;
+    }
+  });
+
+  if (!(validStart && validEnd)) {
+    alert('Invalid start/end nodes');
+  }
+
+  console.log('valid inputs!');
+  return validStart && validEnd && valid;
+}
+
 function generate(event) {
   const form = document.getElementById("input-form");
   const formData = new FormData(form);
@@ -22,15 +68,91 @@ function generate(event) {
     }
   }
 
-  // TODO error check data types for schema
-  console.log("SCHEMA:", schema);
+  if (!validate(schema)) {
+    alert('YOU\'RE WRONG!');
+    // TODO clear fields
+    return;
+  }
+
+  renderSFG();
   event.preventDefault();
+}
+
+function renderSFG() {
+  cytoscape({
+    container: document.getElementById('sfg_canvas'), // container to render in
+    elements: { // list of graph elements to start with
+      nodes: [
+        { // node a
+          data: { id: 'x0' }
+        },
+        { // node b
+          data: { id: 'x1' }
+        },
+        { // node c
+          data: { id: 'x2' }
+        },
+        { // node d
+          data: { id: 'x3' }
+        },
+        { // node d
+          data: { id: 'x4' }
+        }],
+      edges: [
+        { // edge ab
+          data: { id: '1', source: 'x0', target: 'x1' }
+        },
+        { // edge bc
+          data: { id: 'a', source: 'x1', target: 'x2' }
+        },
+        { // edge ab
+          data: { id: 'b', source: 'x2', target: 'x3' }
+        },
+        { // edge ab
+          data: { id: '123', source: 'x3', target: 'x4' }
+        },
+        { // edge ab
+          data: { id: 'd', source: 'x4', target: 'x3' }
+        },
+        { // edge ab
+          data: { id: 'e', source: 'x4', target: 'x0' }
+        }]
+    },
+    style: [ // the stylesheet for the graph
+      {
+        selector: 'node',
+        style: {
+          'background-color': '#666',
+          'label': 'data(id)'
+        }
+      },
+      {
+        selector: 'edge',
+        style: {
+          'width': 3,
+          'curve-style': 'bezier',
+          'control-point-distance': '50px',
+          'control-point-weight': '0.7', // '0': curve towards source node, '1': towards target node.
+          'edge-distances': 'intersection',
+          'line-color': '#ccc',
+          'target-arrow-color': '#ccc',
+          'target-arrow-shape': 'triangle',
+          'label': 'data(id)'
+        }
+      }
+    ],
+    layout: {
+      name: 'grid',
+      rows: 1
+    }
+  });
 }
 
 function addEquation() {
   const ind = equations.childElementCount - 1;
   const newEqnInput = document.createElement('div');
   newEqnInput.id = `equation-input-${ind}`;
+  newEqnInput.style.marginBottom = '5px';
 
   const input = document.createElement('input');
   const label = document.createElement('label');
@@ -40,6 +162,9 @@ function addEquation() {
   input.name = input.id;
   label.for = input.id;
   span.innerHTML = 'remove';
+  span.style.paddingLeft = '5px';
+  span.style.fontStyle = 'italic';
+  span.style.cursor = 'pointer';
   span.onclick = () => {
     document.getElementById(newEqnInput.id).remove();
   };
