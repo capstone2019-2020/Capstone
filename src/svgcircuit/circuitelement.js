b9313379af414a8d6a7c8fc7c3eac6d54fc6c13a
Patricia <patsmrkt@gmail.com>
2020-03-08 00:22:09 -0500

    adding independent voltage/current source symbol

--- src/svgcircuit/circuitelement.js
+++ src/svgcircuit/circuitelement.js
@@ -8,8 +8,6 @@ const _NODE_RADIUS_ = 4;
 const _LINE_WIDTH_ = 2;
 const _DEFAULT_COLOR_ = '#000';
 
-const _RESISTOR_SLOPE = 0.5;
-
 /*
  * Generic SVG element create functions
  */
@@ -31,19 +29,15 @@ const create = (type, id, coord1, coord2) => {
   coord1: (${coord1.x}, ${coord1.y}), coord2: (${coord2.x}, ${coord2.y})`);
 
   let element = createSVGGroup();
-  createNodes(coord1, coord2).forEach( n => element.appendChild(n));
   switch(type) {
     case _RESISTOR_TYPE_:
-      element.appendChild(
-        Resistor(id, coord1, coord2));
+      element = Resistor(id, coord1, coord2);
       break;
     case _INDEP_VOLTAGE_TYPE_:
-      element.appendChild(
-        IndependentVoltage(id, coord1, coord2));
+      element = IndependentVoltage(id, coord1, coord2);
       break;
     case _INDEP_CURRENT_TYPE_:
-      element.appendChild(
-        IndependentCurrent(id, coord1, coord2));
+      element = IndependentCurrent(id, coord1, coord2);
       break;
     default:
       break;
@@ -70,7 +64,7 @@ const createLines = (coord1, coord2, fract) => {
 
   /* Case 1: horizontal line */
   if (y1 === y2) {
-    const width = Math.abs(x2 - x1);
+    const width = x2 - x1;
 
     /* line 1 */
     line_pos1 = {x: x1, y: y1};
@@ -85,7 +79,7 @@ const createLines = (coord1, coord2, fract) => {
   }
   /* Case 2: vertical line */
   else {
-    const height = Math.abs(y2 - y1);
+    const height = y2 - y1;
 
     /* line 1 */
     line_pos1 = {x: x1, y: y1};
@@ -113,146 +107,261 @@ const Node = (coord) => {
   return node;
 };
 
-const Line = (coord1, coord2) => {
+const Line = (coord1, coord2, width=_LINE_WIDTH_) => {
   let line = createSVGElem('line');
   line.setAttribute('x1', coord1.x);
   line.setAttribute('x2', coord2.x);
   line.setAttribute('y1', coord1.y);
   line.setAttribute('y2', coord2.y);
-  line.setAttribute('stroke-width', _LINE_WIDTH_);
+  line.setAttribute('stroke-width', width);
   line.setAttribute('stroke', _DEFAULT_COLOR_);
   return line;
 };
 
+const Circle = (centre, radius) => {
+  let circle = createSVGElem('circle');
+  circle.setAttribute('r', radius);
+  circle.setAttribute('cx', centre.x);
+  circle.setAttribute('cy', centre.y);
+  circle.setAttribute('fill', 'none');
+  circle.setAttribute('stroke-width', _LINE_WIDTH_);
+  circle.setAttribute('stroke', _DEFAULT_COLOR_);
+  return circle;
+};
+
+const Path = (d) => {
+  let path = createSVGElem('path');
+  path.setAttribute('d', d);
+  path.setAttribute('stroke-width', _LINE_WIDTH_);
+  path.setAttribute('stroke', _DEFAULT_COLOR_);
+  path.setAttribute('fill', 'none');
+  path.setAttribute('fill-opacity', null);
+  return path;
+};
+
+const Text = (msg, coord, size='1em') => {
+  let text = createSVGElem('text');
+  text.setAttribute('x', coord.x);
+  text.setAttribute('y', coord.y);
+  text.setAttribute('font-size', size);
+  text.setAttribute('font-family', 'Helvetica sans-serif');
+  text.setAttribute('text-anchor', 'middle');
+  text.appendChild(
+    document.createTextNode(msg)
+  );
+  return text;
+};
+
 /**
- * Create RESISTOR element - made of 3 SVG Elements
- * -  2 lines
- * -  Middle zig-zag portion
+ * FUNCTIONS TO GENERATE CIRCUIT ELEMENT SYMBOLS
+ * ONE FOR EACH TYPE OF ELEMENT
+ */
+
+/**
+ * Get the boundaries of the element symbols
+ * Required when creating the actual symbols
  *
- * @param id
+ * o------ <   > -------o
+ * Returns start (left arrow) and end (right arrow)
+ * SVG coordinates
+ *
  * @param coord1
  * @param coord2
- * @returns {SVGGElement}
- * @constructor
+ * @param fract
+ * @returns {{start: {x: null, y: null}, end: {x: null, y: null}}}
  */
-const resistorSymbol = (coord1, coord2, fract) => {
+const getSymbolBounds = (coord1, coord2, fract) => {
   /* Get fraction of line that the symbol has to fit in */
   let x1 = parseFloat(coord1.x), y1 = parseFloat(coord1.y);
   let x2 = parseFloat(coord2.x), y2 = parseFloat(coord2.y);
 
-  let d = 'M';
-  let startx, starty, endx, endy;
+  let start = {x: null, y: null};
+  let end = {x: null, y: null};
   if (isHorizontal(coord1, coord2)) {
     const width = x2- x1;
-    startx = x1 + width*fract;
-    starty = y1;
-    endx = x2 - width*fract;
+    start.x = x1 + width*fract;
+    end.x = x2 - width*fract;
 
-    const interval = (endx - startx)/6;
-    let i=0, x, y;
-    for (; i<7; i++) {
-      x = startx + i * interval;
-      y = i%2 === 0 ? starty : starty - 30;
-      d += `${x},${y} `;
-    }
+    start.y = y1;
+    end.y = y2;
 
   } else {
     const height = y2 - y1;
-    startx = x1;
-    starty = y1 + height*fract;
-    endx = x2;
-    endy = y2 - height*fract;
-
+    start.x = x1;
+    start.y = y1 + height*fract;
+    end.x = x2;
+    end.y = y2 - height*fract;
   }
-
-  let symbol = createSVGElem('path');
-  symbol.setAttribute('d', d);
-  symbol.setAttribute('stroke-width', _LINE_WIDTH_);
-  symbol.setAttribute('stroke', _DEFAULT_COLOR_);
-  symbol.setAttribute('fill', 'none');
-  symbol.setAttribute('fill-opacity', null);
-  return symbol;
+  return {start, end};
 };
 
+const resistorSymbol = (start, end) => {
 
-const Resistor = (id, coord1, coord2) => {
-  console.log('CREATING RESISTOR...');
-  let resistor = createSVGGroup();
-  createLines(coord1, coord2, 0.35).forEach(
-    l => resistor.appendChild(l)
-  );
-  resistor.appendChild(
-    resistorSymbol(coord1, coord2, 0.35));
-  return resistor;
+  /*
+   * interval is the num pixels between points on the
+   * path
+   * _____/\/\/\_____
+   *
+   * startx is the start coordinate of the symbol on the
+   * axis that the resistor is lying on
+   * -  Not necessarily equivalent to start.x
+   * -  For vertically oriented resistors startx = start.y
+   *    because the resistor is oriented on the y axis
+   * -  Note to patricai: pls find a better variable name
+   *
+   * starty is the start coordinate of the symbol on the axis
+   * that the spikes will be growing towards
+   * -  Same note as startx: not necessarily the same as start.y
+   */
+  let interval, startx, starty;
+  let horiz = false;
+  if (isHorizontal(start, end)) {
+    interval = (end.x - start.x)/6;
+    startx = start.x;
+    starty = start.y;
+    horiz = true;
+  } else {
+    interval = (end.y - start.y)/6;
+    startx = start.y;
+    starty = start.x;
+  }
+
+  /*
+   * For each interval, compute the coordinate on the path
+   * (7 total points)
+   * horizontal and vertically aligned resistors use the
+   * same equations except x and y equations are flipped,
+   * therefore have to check for this condition
+   */
+  let i=0, x, y;
+  let d = 'M';
+  for (; i<7; i++) {
+    x = startx + i * interval;
+    y = i%2 === 0 ? starty : starty - 30;
+    d += horiz ? `${x},${y} ` : `${y},${x} `;
+  }
+
+  return Path(d);
 };
 
 /**
- * Create independent voltage source - 5 SVG elements
- * -  2 lines
- * -  1 circle
- * -  2 text elements - [+/-]
+ * Circle symbol for independent sources
+ * NOTE: there will be separate functions to create
+ * the symbols inside the circle
+ * (+/-) -> voltage source
+ * -> -> current source
+ * @param start
+ * @param end
+ * @returns {any}
+ */
+const independentSymbol = (start, end) => {
+  let centre;
+  if (isHorizontal(start, end)) {
+    length = end.x - start.x;
+    centre = { x: start.x + length, y: start.y };
+  } else {
+    length = end.y - start.y;
+    centre = { x: start.x, y: start.y + (end.y - start.y)/2}
+  }
+
+  return Circle(centre, Math.abs(length/2));
+};
+
+const voltageSymbol = (pos, neg) => {
+  const L='l', R='r', U='u', D='d';
+  const OFFSET = 5;
+  const font_size = '2em';
+  const horiz = isHorizontal(pos, neg);
+
+  let orientation, center, msg, rotate=false;
+  if (horiz) {
+    center = {x: pos.x + (neg.x - pos.x)/2, y: pos.y + OFFSET };
+    orientation = pos.x > neg.x ? R : L;
+  } else {
+    center = {x: pos.x, y: pos.y + OFFSET +(neg.y - pos.y)/2 };
+    orientation = pos.y > neg.y ? D : U;
+    rotate = true;
+  }
+
+  msg = orientation === D || orientation == R ? '- +' : '+ -';
+  let symbol = Text(msg, center, font_size);
+  if (rotate)
+    symbol.setAttribute('transform', `rotate(90, ${center.x}, ${center.y - OFFSET})`);
+  return symbol;
+};
+
+const currentSymbol = (pos, neg) => {
+  const L='l', R='r', U='u', D='d';
+  const OFFSET = 10;
+  let negx=neg.x, negy=neg.y, posx=pos.x, posy=pos.y, orientation;
+  const horiz = isHorizontal(pos, neg);
+  if (horiz) {
+    orientation = pos.x > neg.x ? R : L;
+    negx = orientation === R ? neg.x + OFFSET : neg.x - OFFSET;
+    posx = orientation === R ? pos.x - OFFSET : pos.x + OFFSET;
+  } else {
+    orientation = pos.y > neg.y ? D : U;
+    negy = orientation === D ? neg.y + OFFSET : neg.y - OFFSET;
+    posy = orientation === D ? pos.y - OFFSET : pos.y + OFFSET;
+  }
+
+
+  let symbol = Line({x: negx, y: negy}, {x: posx, y: posy }, 5);
+  symbol.setAttribute('marker-end', 'url(#arrow)');
+  return symbol;
+};
+
+/**
+ * Generate template for a generic circuit element
+ * - 2 lines + 2 nodes
+ * o-----     -----o
  *
  * @param id
  * @param coord1
  * @param coord2
- * @returns {SVGGElement}
+ * @param fract - % of line from coord1 -> coord2 that is used up by the 2 lines
  * @constructor
  */
-const independentSymbol = (coord1, coord2, fract) => {
-  let x1 = parseFloat(coord1.x), y1 = parseFloat(coord1.y);
-  let x2 = parseFloat(coord2.x), y2 = parseFloat(coord2.y);
-  let width, centre, startx, starty, endx, endy;
-  if (isHorizontal(coord1, coord2)) {
-    const width = Math.abs(x2 - x1);
-    startx = x1 + width*fract;
-    starty = y1;
-    endx = x2 - width*fract;
-
-    length = Math.abs(endx - startx);
-    centre = { x: startx + (endx - startx)/2, y: starty };
-  } else {
-    const height = Math.abs(y2 - y1);
-    startx = x1;
-    starty = y1 + height*fract;
-    endy = y2 - height*fract;
-
-    length = Math.abs(endy - starty);
-    centre = { x: startx, y: starty + (endy - starty)/2}
-  }
-
-  let symbol = createSVGElem('circle');
-  symbol.setAttribute('r', length/2);
-  symbol.setAttribute('cx', centre.x);
-  symbol.setAttribute('cy', centre.y);
-  symbol.setAttribute('fill', 'none');
-  symbol.setAttribute('stroke-width', _LINE_WIDTH_);
-  symbol.setAttribute('stroke', _DEFAULT_COLOR_);
-  return symbol;
+const GenericElement = (id, coord1, coord2, fract) => {
+  let element = createSVGGroup();
+  createNodes(coord1, coord2).forEach(
+    n => element.appendChild(n)
+  );
+  createLines(coord1, coord2, fract).forEach(
+    l => element.appendChild(l)
+  );
+  return element;
 };
 
-const voltageSigns = (coord1, coord2, fract) => {
+const Resistor = (id, coord1, coord2) => {
+  console.log('CREATING RESISTOR...');
+  let resistor = GenericElement(id, coord1, coord2, 0.35);
 
+  const {start, end} = getSymbolBounds(coord1, coord2, 0.35);
+  resistor.appendChild(
+    resistorSymbol(start, end));
+  return resistor;
 };
+
 
 const IndependentVoltage = (id, coord1, coord2) => {
   console.log('CREATING INDEPENDENT VOLTAGE SOURCE...');
-  let voltage_src = createSVGGroup();
-  createLines(coord1, coord2, 1/3).forEach(
-    l => voltage_src.appendChild(l)
-  );
-  voltage_src.appendChild(independentSymbol(coord1, coord2, 1/3));
+  let voltage_src = GenericElement(id, coord1, coord2, 1/3);
+
+  const {start, end} = getSymbolBounds(coord1, coord2, 1/3);
+  voltage_src.appendChild(independentSymbol(start, end));
+  voltage_src.appendChild(voltageSymbol(start, end));
   return voltage_src;
 };
 
 const IndependentCurrent = (id, coord1, coord2) => {
   console.log('CREATING INDEPENDENT CURRENT...');
-  let current_src = createSVGGroup();
-  createLines(coord1, coord2, 1/3).forEach(
-    l => current_src.appendChild(l)
-  );
-  current_src.appendChild(independentSymbol(coord1, coord2, 1/3));
-  return current_src;
+  let current_src = GenericElement(id, coord1, coord2, 1/3);
 
+  const {start, end} = getSymbolBounds(coord1, coord2, 1/3);
+  current_src.appendChild(independentSymbol(start, end));
+  current_src.appendChild(currentSymbol(start, end));
+  return current_src;
 };
 
 const Element = { create };
--- src/svgcircuit/svgcircuit.js
+++ src/svgcircuit/svgcircuit.js
@@ -7,12 +7,13 @@ const generateCircuit = (canvas_id) => {
 	let circuit_canvas = document.getElementById(canvas_id);
 	let prop, elem, elem_id, elem_type,
 		positive_node, negative_node;
+
 	circuit_elems.forEach( c => {
 		prop = c.split(' ');
 		elem_id = prop[0];
 		elem_type = elem_id.charAt(0);
-		positive_node = {x: prop[4], y: prop[5]};
-		negative_node = {x: prop[6], y: prop[7]};
+		positive_node = {x: prop[4], y: prop[5], id: prop[1]};
+		negative_node = {x: prop[6], y: prop[7], id: prop[2]};
 
 		elem = Element.create(elem_type, elem_id,
 			positive_node, negative_node);
